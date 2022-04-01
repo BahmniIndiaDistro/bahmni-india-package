a0581cb55417a5ae812c17c6e0d390e3aae7dc1a
SanoferSameera <sanofersameera25@gmail.com>
2022-04-01 18:15:14 +0530

    BAH-1439 | Refactor. updated dockerfile and scripts for unzipping
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

--- bahmni-docker/build_scripts/bahmni-emr/docker_build.sh
+++ bahmni-docker/build_scripts/bahmni-emr/docker_build.sh
@@ -10,17 +10,6 @@ cp default-config/target/default_config.zip bahmni-india-package/bahmni-emr/reso
 
 cd bahmni-india-package/bahmni-emr
 
-rm -rf build/
-mkdir build/
-cd build && mkdir openmrs-modules/ && cd ..
-
-# Copy NDHM OMODS
-cp resources/fhir2-omod-1.0.0-SNAPSHOT.omod build/openmrs-modules/
-cp resources/hipmodule-omod-0.1-SNAPSHOT.omod build/openmrs-modules/
-
-# Unzipping Default Config
-unzip -q -u -d build/default_config resources/default_config.zip
-
 # Building Docker images
 OPENMRS_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
 docker build -t bahmni/openmrs:${OPENMRS_IMAGE_TAG} -f docker/Dockerfile  . --no-cache
--- bahmni-docker/build_scripts/bahmni-web/docker_build.sh
+++ bahmni-docker/build_scripts/bahmni-web/docker_build.sh
@@ -6,14 +6,6 @@ cd default-config && scripts/package.sh && cd ..
 cp default-config/target/default_config.zip bahmni-india-package/bahmni-web/resources/
 
 cd bahmni-india-package/bahmni-web
-rm -rf build/
-mkdir build/
-
-# Unzipping Resources
-unzip -q -u -d build/default_config resources/default_config.zip
-unzip -q -u -d build/NDHMReact resources/ndhm-react.zip
-cp -a build/NDHMReact/build/. build/NDHMReact/
-rm -rf build/NDHMReact/build/
 
 #Building Docker images
 BAHMNI_WEB_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
