245f5fac00540a35e02359dfbe449efc6508453d
Arjun G <91885483+Arjun-Go@users.noreply.github.com>
2023-02-24 20:48:03 +0530

    Update modify-server-properties.sh

--- bahmni-emr/package/docker/resources/modify-server-properties.sh
+++ bahmni-emr/package/docker/resources/modify-server-properties.sh
@@ -3,12 +3,12 @@ set -e
 
 sed '/module.allow_web_admin/a\
 c3p0.min_size=0\
-c3p0.max_size=150\
-c3p0.max_statements=0\
-c3p0.maxStatementsPerConnection=0\
+c3p0.max_size=200\
+c3p0.max_statements=100\
+c3p0.maxStatementsPerConnection=1000\
 c3p0.maxIdleTime=5\
 c3p0.maxIdleTimeExcessConnections=5\
-c3p0.numHelperThreads=10' ./startup-init.sh > ./startup-init-temp.sh
+c3p0.numHelperThreads=20' ./startup-init.sh > ./startup-init-temp.sh
 
 rm ./startup-init.sh
 mv ./startup-init-temp.sh ./startup-init.sh
